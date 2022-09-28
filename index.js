2042dea8e6a8157695a60c32b2189d89abaac317
Satender Yadav <satenderyadav@Sams-Machine.local>
2022-09-28 15:23:29 -0400

    added seneca methods

--- index.js
+++ index.js
@@ -24,3 +24,63 @@ var plugin = function (options) {
 module.exports = plugin;
 
 
+
+var seneca = require("seneca")();
+seneca.use(plugin);
+seneca.use('seneca-entity');
+
+seneca.add('role:api, cmd:add-user', function (args, done) {
+    console.log("--> cmd:add-user");
+    var employee = {
+        firstname: args.firstname,
+        lastname: args.lastname,
+        position: args.position
+    }
+    console.log("--> employee: " + JSON.stringify(employee));
+    seneca.act({ role: 'employee', cmd: 'add', data: employee }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:api, cmd:get-all-users', function (args, done) {
+    console.log("--> cmd:get-all-users");
+    seneca.act({ role: 'employee', cmd: 'get-all' }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:api, cmd:get-user', function (args, done) {
+    console.log("--> cmd:get-user, args.user_id: " + args.user_id);
+    seneca.act({ role: 'employee', cmd: 'get', data: { user_id: args.user_id } }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+
+seneca.add('role:api, cmd:delete-user', function (args, done) {
+    console.log("--> cmd:delete-user, args.user_id: " + args.user_id);
+    seneca.act({ role: 'employee', cmd: 'delete', data: { user_id: args.user_id } }, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
+});
+
+seneca.add('role:api, cmd:delete-all-users', function (args, done) {
+    done(null, { cmd: "delete-all-users" });
+});
+
+seneca.act('role:web', {
+    use: {
+        prefix: '/abservice',
+        pin: { role: 'api', cmd: '*' },
+        map: {
+            'add-user': { GET: true },
+            'get-all-users': { GET: true },
+            'get-user': { GET: true, },
+            'delete-user': { GET: true, }
+        }
+    }
+})

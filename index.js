9149d2647b8b35ce3c3af588830d77e76f68d7ce
Satender Yadav <satenderyadav@Sams-Machine.local>
2022-09-28 15:22:15 -0400

    fixed dependencies

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('here I am yoo');
+
+var plugin = function (options) {
+    var seneca = this;
+
+    seneca.add({ role: 'employee', cmd: 'add' }, function (msg, respond) {
+        this.make('employee').data$(msg.data).save$(respond);
+    });
+
+    seneca.add({ role: 'employee', cmd: 'get' }, function (msg, respond) {
+        this.make('employee').load$(msg.data.user_id, respond);
+    });
+
+    seneca.add({ role: 'employee', cmd: 'get-all' }, function (msg, respond) {
+        this.make('employee').list$({}, respond);
+    });
+
+    seneca.add({ role: 'employee', cmd: 'delete' }, function (msg, respond) {
+        this.make('employee').remove$(msg.data.user_id, respond);
+    });
+
+
+}
+
+module.exports = plugin;
+
+
